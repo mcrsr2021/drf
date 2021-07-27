3abbb299fbe31683ac3527d7f1a952054b2e69d0
mcrsr2021 <mcrsr2021@gmail.com>
2021-07-28 01:00:10 +0530

    Ninth Commit

--- testapp/views.py
+++ testapp/views.py
@@ -1,27 +1,13 @@
 from testapp.models import Employee
 from testapp.serializers import EmployeeSerializer
-from rest_framework import mixins,generics
+from rest_framework import generics
 
 
-class EmployeeList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+class EmployeeList(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
-    def get(self,request,*args,**kwargs):
-        return self.list(request,*args,**kwargs)
 
-    def post(self,request,*args,**kwargs):
-        return self.create(request,*args,**kwargs)
-
-class EmployeeDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
