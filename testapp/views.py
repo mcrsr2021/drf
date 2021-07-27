ec096368b943eb23ba44915de0bf1ef8942b3321
mcrsr2021 <mcrsr2021@gmail.com>
2021-07-27 17:37:18 +0530

    Second Commit

--- testapp/views.py
+++ testapp/views.py
@@ -2,6 +2,7 @@ from django.views.decorators import csrf
 from testapp.models import Employee
 from testapp.forms import EmployeeForm
 from django.http import HttpResponse
+from django.core.serializers import serialize
 import json
 from django.views.decorators.csrf import csrf_exempt
 
@@ -12,14 +13,15 @@ def employee_detail(request,pk):
     except Employee.DoesNotExist:
         json_data = json.dumps({'msg':'The requested resource not available'})
         return HttpResponse(json_data,content_type='application/json',status=404)
+        
     if request.method == 'GET':
-        emp_data = {
-            'eno':emp.eno,
-            'ename':emp.ename,
-            'esal':emp.esal,
-            'eaddr':emp.eaddr
-        }
-        json_data = json.dumps(emp_data)
+        json_data = serialize('json',[emp,])
+        p_data = json.loads(json_data)
+        final_list = []
+        for obj in p_data:
+            emp_data = obj['fields']
+            final_list.append(emp_data)
+        json_data = json.dumps(final_list)
         return HttpResponse(json_data,content_type='application/json',status=200)
     
     elif request.method == 'PUT':
@@ -30,12 +32,8 @@ def employee_detail(request,pk):
         except:
             json_data = json.dumps({'msg':'Please Provide data in valid json format'})
             return HttpResponse(json_data,content_type='application/json',status=400)
-        original_data = {
-            'eno':emp.eno,
-            'ename':emp.ename,
-            'esal':emp.esal,
-            'eaddr':emp.eaddr
-        }
+        from django.forms.models import model_to_dict
+        original_data = model_to_dict(emp)
         original_data.update(p_data)
         form = EmployeeForm(original_data)
         if form.is_valid():
@@ -45,6 +43,7 @@ def employee_detail(request,pk):
         if form.errors:
             json_data = json.dumps(form.errors)
             return HttpResponse(json_data,content_type='application/json',status=400)
+    
     elif request.method == 'DELETE':
         emp.delete()
         return HttpResponse(status=204)
@@ -53,17 +52,15 @@ def employee_detail(request,pk):
 def employee_list(request):
     if request.method == 'GET':
         emp = Employee.objects.all()
-        emp_data = []
-        for e in emp:
-            e = {
-                'eno':e.eno,
-                'ename':e.ename,
-                'esal':e.esal,
-                'eaddr':e.eaddr
-                }
-            emp_data.append(e)
-        json_data = json.dumps(emp_data)
+        json_data = serialize('json',emp)
+        p_data = json.loads(json_data)
+        final_list = []
+        for obj in p_data:
+            emp_data = obj['fields']
+            final_list.append(emp_data)
+        json_data = json.dumps(final_list)
         return HttpResponse(json_data,content_type='application/json',status=200)
+
     elif request.method == 'POST':
         b_data = request.body  #binary data
         json_data = b_data.decode('utf-8') # json data
@@ -79,4 +76,3 @@ def employee_list(request):
         if form.errors:
             json_data = json.dumps(form.errors)
             return HttpResponse(json_data,content_type='application/json',status=400)
-        
